49917413ff5eba9c2f42a478218b140949b21a87
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-17 19:05:51 +0500

    More done

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -6,17 +6,27 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
     word_vec_uppercase.sort_unstable();
     let word_uppercase_sorted = vec_uppercase_to_string(&word_vec_uppercase);
 
-    possible_anagrams.into_iter().filter(|&anagram| {
-        let mut anagram_vec_uppercase = to_vec_uppercase(anagram);
-        let anagram_uppercase_string = vec_uppercase_to_string(&anagram_vec_uppercase);
-        if anagram_uppercase_string == word_uppercase_string {
-            false
-        } else {
-            anagram_vec_uppercase.sort_unstable();
-            let anagram_uppercase_sorted = vec_uppercase_to_string(&anagram_vec_uppercase);
-            anagram_uppercase_sorted == word_uppercase_sorted
-        }
-    }).map(|&s| s).collect()
+    possible_anagrams
+        .iter()
+        .filter(|anagram| check_if_anagram(anagram, &word_uppercase_string, &word_uppercase_sorted))
+        .map(|&s| s)
+        .collect()
+}
+
+fn check_if_anagram(
+    anagram: &str,
+    word_uppercase_string: &String,
+    word_uppercase_sorted: &String,
+) -> bool {
+    let mut anagram_vec_uppercase = to_vec_uppercase(anagram);
+    let anagram_uppercase_string = vec_uppercase_to_string(&anagram_vec_uppercase);
+    if anagram_uppercase_string == *word_uppercase_string {
+        false
+    } else {
+        anagram_vec_uppercase.sort_unstable();
+        let anagram_uppercase_sorted = vec_uppercase_to_string(&anagram_vec_uppercase);
+        anagram_uppercase_sorted == *word_uppercase_sorted
+    }
 }
 
 fn to_vec_uppercase(s: &str) -> Vec<char> {
