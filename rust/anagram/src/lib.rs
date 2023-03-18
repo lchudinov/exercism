39e104368e4f4162940673425e519a4013311739
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-18 16:01:02 +0500

    anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,21 +1,22 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let word_uppercase_sorted = to_uppercase_sorted(word);
     let word_uppercase = word.to_uppercase();
+    let word_uppercase_sorted = to_sorted(&word_uppercase);
 
     possible_anagrams
         .iter()
         .filter(|anagram| {
-            anagram.to_uppercase() != word_uppercase
-                && to_uppercase_sorted(anagram) == word_uppercase_sorted
+            let anagram_uppercase = anagram.to_uppercase();
+            anagram_uppercase != word_uppercase
+                && to_sorted(&anagram_uppercase) == word_uppercase_sorted
         })
         .cloned()
         .collect()
 }
 
-fn to_uppercase_sorted(s: &str) -> String {
-    let mut v: Vec<char> = s.chars().map(|c| c.to_uppercase()).flatten().collect();
+fn to_sorted(s: &String) -> String {
+    let mut v: Vec<char> = s.chars().collect();
     v.sort_unstable();
     v.into_iter().collect()
 }
