4b3d2ed1cfaee4d9a4aea89e0f1d7efdbfe55db2
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-18 20:39:34 +0500

    sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,25 +7,12 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    if first_list == second_list {
-        Comparison::Equal
-    } else if first_list.is_empty() {
-        Comparison::Sublist
-    } else if second_list.is_empty() {
-        Comparison::Superlist
-    } else if first_list.len() > second_list.len() {
-        if first_list.windows(second_list.len()).any(|w| w == second_list) {
-            Comparison::Superlist
-        } else {
-            Comparison::Unequal
-        }
-    } else if first_list.len() < second_list.len() {
-        if second_list.windows(first_list.len()).any(|w| w == first_list) {
-            Comparison::Sublist
-        } else {
-            Comparison::Unequal
-        }
-    } else {
-        Comparison::Unequal
+    match (first_list.len(), second_list.len()) {
+        (0, 0) => Comparison::Equal,
+        (0, _) => Comparison::Sublist,
+        (_, 0) => Comparison::Superlist,
+        (a, b) if a > b => if first_list.windows(b).any(|w| w == second_list) {Comparison::Superlist} else {Comparison::Unequal},
+        (a, b) if a < b => if second_list.windows(a).any(|w| w == first_list) {Comparison::Sublist} else {Comparison::Unequal},
+        (_, _) => if first_list == second_list {Comparison::Equal} else {Comparison::Unequal},
     }
 }
