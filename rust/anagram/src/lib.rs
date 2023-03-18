f15456117f4b03babd6d3174b0d9817ac438d39e
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-18 16:02:13 +0500

    anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,20 +2,20 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let word_uppercase = word.to_uppercase();
-    let word_uppercase_sorted = to_sorted(&word_uppercase);
+    let word_uppercase_sorted = sort_string(&word_uppercase);
 
     possible_anagrams
         .iter()
         .filter(|anagram| {
             let anagram_uppercase = anagram.to_uppercase();
             anagram_uppercase != word_uppercase
-                && to_sorted(&anagram_uppercase) == word_uppercase_sorted
+                && sort_string(&anagram_uppercase) == word_uppercase_sorted
         })
         .cloned()
         .collect()
 }
 
-fn to_sorted(s: &String) -> String {
+fn sort_string(s: &String) -> String {
     let mut v: Vec<char> = s.chars().collect();
     v.sort_unstable();
     v.into_iter().collect()
