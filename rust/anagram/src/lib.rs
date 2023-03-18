b45468e46c2adbc62e0b72477ac9407fb217d295
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-18 09:35:34 +0500

    anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,14 +1,12 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut word_vec_uppercase = to_vec_uppercase(word);
-    let word_uppercase_string = vec_uppercase_to_string(&word_vec_uppercase);
-    word_vec_uppercase.sort_unstable();
-    let word_uppercase_sorted = vec_uppercase_to_string(&word_vec_uppercase);
+    let word_uppercase_sorted = to_sorted_uppercase_string(word);
+    let word_uppercase = word.to_uppercase();
 
     possible_anagrams
-        .iter()
-        .filter(|anagram| check_if_anagram(anagram, &word_uppercase_string, &word_uppercase_sorted))
+        .into_iter()
+        .filter(|anagram| check_if_anagram(anagram, &word_uppercase, &word_uppercase_sorted))
         .map(|&s| s)
         .collect()
 }
@@ -18,21 +16,17 @@ fn check_if_anagram(
     word_uppercase_string: &String,
     word_uppercase_sorted: &String,
 ) -> bool {
-    let mut anagram_vec_uppercase = to_vec_uppercase(anagram);
-    let anagram_uppercase_string = vec_uppercase_to_string(&anagram_vec_uppercase);
-    if anagram_uppercase_string == *word_uppercase_string {
+    let anagram_uppercase = anagram.to_uppercase();
+    if anagram_uppercase == *word_uppercase_string {
         false
     } else {
-        anagram_vec_uppercase.sort_unstable();
-        let anagram_uppercase_sorted = vec_uppercase_to_string(&anagram_vec_uppercase);
+        let anagram_uppercase_sorted = to_sorted_uppercase_string(anagram);
         anagram_uppercase_sorted == *word_uppercase_sorted
     }
 }
 
-fn to_vec_uppercase(s: &str) -> Vec<char> {
-    s.chars().map(|c| c.to_uppercase()).flatten().collect()
-}
-
-fn vec_uppercase_to_string(v: &Vec<char>) -> String {
+fn to_sorted_uppercase_string(s: &str) -> String {
+    let mut v: Vec<char> = s.chars().map(|c| c.to_uppercase()).flatten().collect();
+    v.sort_unstable();
     v.into_iter().collect()
 }
