fa98478371299c9cb267da1143fa4398aac946ed
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-18 20:31:35 +0500

    Sublist

---
+++ rust/sublist/src/lib.rs
@@ -0,0 +1,31 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    if first_list == second_list {
+        Comparison::Equal
+    } else if first_list.is_empty() {
+        Comparison::Sublist
+    } else if second_list.is_empty() {
+        Comparison::Superlist
+    } else if first_list.len() > second_list.len() {
+        if first_list.windows(second_list.len()).any(|w| w == second_list) {
+            Comparison::Superlist
+        } else {
+            Comparison::Unequal
+        }
+    } else if first_list.len() < second_list.len() {
+        if second_list.windows(first_list.len()).any(|w| w == first_list) {
+            Comparison::Sublist
+        } else {
+            Comparison::Unequal
+        }
+    } else {
+        Comparison::Unequal
+    }
+}
