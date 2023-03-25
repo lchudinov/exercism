8722301480f983862895b2d6ec3a86215b82b6da
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-03-25 10:31:14 +0500

    separate function

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -3,13 +3,24 @@ use std::collections::HashMap;
 pub fn frequency(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
     let mut m = HashMap::<char, usize>::new();
     for &s in input {
-        for ch in s
-            .chars()
-            .filter(|ch| ch.is_alphabetic())
-            .flat_map(|ch| ch.to_lowercase())
-        {
-            m.entry(ch).and_modify(|count| *count += 1).or_insert(1);
+        let mx = freq(s);
+        for (&key, &value) in mx.iter() {
+            m.entry(key)
+                .and_modify(|count| *count += value)
+                .or_insert(value);
         }
+    }
+    m
+}
+
+fn freq(input: &str) -> HashMap<char, usize> {
+    let mut m = HashMap::<char, usize>::new();
+    for ch in input
+        .chars()
+        .filter(|ch| ch.is_alphabetic())
+        .flat_map(|ch| ch.to_lowercase())
+    {
+        m.entry(ch).and_modify(|count| *count += 1).or_insert(1);
     }
     m
 }
