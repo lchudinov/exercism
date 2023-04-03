cf1199ba4b80149c555764fdeeb7f8a7cb2c0f64
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-04-03 21:35:23 +0500

    collatz

---
+++ rust/collatz-conjecture/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn collatz(n: u64) -> Option<u64> {
+    let mut steps = 0;
+    let mut num = n;
+    if n == 0 {
+        return None;
+    }
+    while num != 1 {
+        if num % 2 == 0 {
+            num /= 2;
+        } else if let Some(Some(res)) = 3u64.checked_mul(num).map(|v| v.checked_add(1)) {
+            num = res;
+        } else {
+            return None;
+        }
+        steps += 1;
+    }
+    Some(steps)
+}
