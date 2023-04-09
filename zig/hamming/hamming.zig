3a2c90b22257ef07fd4493b52c3e9ce17502f495
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-04-09 20:03:01 +0500

    Hamming

---
+++ zig/hamming/hamming.zig
@@ -0,0 +1,20 @@
+pub const DnaError = error{
+    EmptyDnaStrands,
+    UnequalDnaStrands,
+};
+
+pub fn compute(first: []const u8, second: []const u8) DnaError!usize {
+    if (first.len == 0 or second.len == 0) {
+        return DnaError.EmptyDnaStrands;
+    }
+    if (first.len != second.len) {
+        return DnaError.UnequalDnaStrands;
+    }
+    var distance: usize = 0;
+    for (first, 0..) |c, i| {
+        if (c != second[i]) {
+            distance += 1;
+        }
+    }
+    return distance;
+}
