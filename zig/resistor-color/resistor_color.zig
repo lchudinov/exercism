81f330c65dc2f01d624b36b11b94c8054b670b65
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-04-11 12:48:54 +0500

    colorband

---
+++ zig/resistor-color/resistor_color.zig
@@ -0,0 +1,20 @@
+pub const ColorBand = enum {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+};
+
+pub fn colorCode(color: ColorBand) usize {
+    return @enumToInt(color);
+}
+
+pub fn colors() []const ColorBand {
+    return &[_]ColorBand{ .black, .brown, .red, .orange, .yellow, .green, .blue, .violet, .grey, .white };
+}
