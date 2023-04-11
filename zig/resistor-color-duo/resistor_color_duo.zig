33749d94e769a371cde89b4e232105ef9c42226c
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-04-11 13:10:03 +0500

    resistor color duo

---
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -0,0 +1,16 @@
+pub const ColorBand = enum(usize) {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+};
+
+pub fn colorCode(colors: [2]ColorBand) usize {
+    return 10 * @enumToInt(colors[0]) + @enumToInt(colors[1]);
+}
