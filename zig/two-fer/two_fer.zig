157b72fa35ab43bd5f1086ed3a46a94d31e7251f
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-04-14 15:40:09 +0500

    two fer 2

--- zig/two-fer/two_fer.zig
+++ zig/two-fer/two_fer.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
 pub fn twoFer(buffer: []u8, name: ?[]const u8) anyerror![]u8 {
-    if (name) |n| {
-        return try std.fmt.bufPrint(buffer, "One for {s}, one for me.", .{n});
-    } else {
-        return try std.fmt.bufPrint(buffer, "One for {s}, one for me.", .{"you"});
-    }
+    return try std.fmt.bufPrint(buffer, "One for {s}, one for me.", .{name orelse "you"});
 }
